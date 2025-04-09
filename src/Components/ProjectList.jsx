ecd44108f09b254bf4d51081bdc042a802b9ed8b
Laura <laura.laukiz@gmail.com>
2025-04-09 13:20:53 +0200

    estilando

--- src/Components/ProjectList.jsx
+++ src/Components/ProjectList.jsx
@@ -1,67 +1,24 @@
 import { Link } from "react-router-dom";
+import "../scss/layout/ProjectList.scss";
 import Hero from "./Hero";
-import imageAvatar from "../images/avatar.webp";
+import Card from "./Card";
+
 
 
 function ProjectList() {
 
   return (
-   
     <>
-     <Hero />
-    <article className="card">
-                         <h2 className="card__projectTitle">
-                             <span className="card__projectTitle--text">
-                                 Personal project card:
-                             </span>
-                         </h2>
-    
-                         <div className="card__author">
-                             <div className="card__authorPhoto"><img src={imageAvatar}   alt="avatar" /></div>
-                             <p className="card__job"></p>
-                             <h3 className="card__name">
-                               
-                             </h3>
-                         </div>
-    
-                         <div className="card__project">
-                             <h3 className="card__name">
-                                
-                             </h3>
-                             <p className="card__slogan">
-                                 
-                             </p>
-                             <h3 className="card__descriptionTitle">
-                                 Product description
-                             </h3>
-                             <p className="card__description">
-                                
-                             </p>
-                             <div className="card__technicalInfo">
-                                 <p className="card__technologies">
-                                     
-                                 </p>
-    
-                                 <a
-                                     className="icon icon__www"
-                                     href=""
-                                     title="Haz click para ver el proyecto online"
-                                     target="_blank"
-                                 >
-                                     Web link
-                                 </a>
-                                 <a
-                                     className="icon icon__github"
-                                     href=""
-                                     title="Haz click para ver el código del proyecto"
-                                     target="_blank"
-                                 >
-                                     GitHub link
-                                 </a>
-                             </div>
-                         </div>
-                     </article>
-                     </>
+        <Hero />
+        <div className="container">
+        <section className="cardList">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </section>
+        </div>
+    </>
   )
 }
 
