f5b1c5d6df3eed29cd503bbad22c3c16143d151c
Judit Casamitjana <juditcasamitjana@gmail.com>
2025-04-11 13:24:44 +0200

    Add relations

--- bd/tables.sql
+++ bd/tables.sql
@@ -21,3 +21,16 @@ CREATE TABLE autor (
 
 ALTER TABLE project ADD COLUMN fk_autor INT NOT NULL;
 ALTER TABLE project ADD FOREIGN KEY (fk_autor) REFERENCES autor(id);
+
+INSERT INTO autor (name, job, autorImage)
+VALUES
+	('Laura', 'FullStack', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXSTblEVkkdJh15jlAbC3FpvuzCKb1o-pQQA&s'),
+    ('Judit', 'FullStack', 'https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_1280.png');
+
+INSERT INTO project (projectName, slogan, demo, repository, technologies, description, projectImage, fk_autor)
+VALUES
+	('Proyectos Molones', 'Confía en el proceso', 'https://www.google.es', 'https://www.google.es', 'HTML, CSS, JS y React', 'Es un proyecto sobre bases de datos', 'https://es.unesco.org/youth/toptips/user/pages/images/home-feature-two_mobile.png', 1),
+    ('Proyectos más molones', 'Hoy me siento un poco column-reverse', 'https://www.google.es', 'https://www.google.es', 'HTML, CSS, JS y React', 'Es un proyecto sobre las dificultades de programar', 'https://assets.asana.biz/m/2729b34d99aa7f91/webimage-article-project-planning-project-design-2x.jpg', 2);
+
+
+    
