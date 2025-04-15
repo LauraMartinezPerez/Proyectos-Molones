817283e4e96041e6ce9438e555187bce1330f2a6
Laura <laura.laukiz@gmail.com>
2025-04-15 13:32:58 +0200

    programando servidor

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+//1. Importar los módulos de NPM que necesito
+const express = require("express");
+const cors = require ("cors");
+
+//2. Crear el servidor
+const server = express();
+
+//3. Configurar el servidor
+server.use(cors());
+/* server.use(express.json({limit: "25mb"})); */
+
+//4. Arrancar el servidor en el puerto
+const port = 4000;
+server.listen(port, () => {
+    console.log("Serever is running on http://localhost:" + port)
+})
+
+//5. Servidor de estaticos
+const staticServerPath = "./web/dist"; //difino donde estan los ficheros a servir, la web
+server.use(express.static(staticServerPath));
+
+
+
